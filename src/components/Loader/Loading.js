74e06a9bad49e367ac8ce625557d82d879ddd1ac
Daniel <conevskidaniel1999@gmail.com>
2022-09-07 16:35:25 +0200

    create loading spinner component

---
+++ src/components/Loader/Loading.js
@@ -0,0 +1,11 @@
+import "./Loading.css";
+
+const Loading = () => {
+    return (
+        <div className="spinner">
+            <div className="spinner__loading">🚀</div>
+        </div>
+    );
+};
+
+export default Loading;
