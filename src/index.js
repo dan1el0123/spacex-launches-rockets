53638fa4f597cc8233a8c4eb00eb0e61d12f1d7e
Daniel <conevskidaniel1999@gmail.com>
2022-09-07 16:37:55 +0200

    integrate and initialize  apollo client

--- src/index.js
+++ src/index.js
@@ -3,14 +3,22 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+const client = new ApolloClient({
+    uri: "https://api.spacex.land/graphql/",
+    cache: new InMemoryCache(),
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        <BrowserRouter>
-            <Routes>
-                <Route path="/*" element={<App />} />
-            </Routes>
-        </BrowserRouter>
+        <ApolloProvider client={client}>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/*" element={<App />} />
+                </Routes>
+            </BrowserRouter>
+        </ApolloProvider>
     </React.StrictMode>
 );
